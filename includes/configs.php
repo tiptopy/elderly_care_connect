72dc06b5922ffe51b26262ff6f8c7c0a5d78183b
Tiptopy <tiptopywafula@gmail.com>
2024-03-10 22:42:30 +0300

    Began donation implementation

---
+++ includes/configs.php
@@ -0,0 +1,3 @@
+<?php
+$SecretKey = "sk_live_697651b447e665bbfe23ee6e0aefb61d7cfc2866"; // Add your secret key here. Remember to change this to your live secret key in production
+$PublicKey = "pk_live_752200c29318008c7146b0f7422bd1ac0ad6ba4a"; // Add your public key here. Remember to change this to your live public key in production
